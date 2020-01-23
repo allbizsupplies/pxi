54dc1ebb4817b4cd70b16fcd3822c19a32507ae1
Brad Treloar <bradley.treloar@gmail.com>
2020-01-23 10:51:44 +1030

    Added price calc operation

---
+++ pxi/operations.py
@@ -0,0 +1,88 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from pxi.exporters import (
+    export_contract_item_task,
+    export_price_changes_report,
+    export_pricelist,
+    export_product_price_task,
+    export_tickets_list
+)
+from pxi.importers import (
+    import_contract_items,
+    import_inventory_items,
+    import_price_region_items,
+    import_price_rules,
+    import_warehouse_stock_items
+)
+from pxi.models import Base, PriceRegionItem, PriceRule
+from pxi.price_calc import (
+    recalculate_contract_prices,
+    recalculate_sell_prices
+)
+
+
+def db_session():
+    db = create_engine('sqlite://')
+    Base.metadata.create_all(db)
+    session = sessionmaker(bind=db)()
+    return session
+
+
+class operations:
+        
+    @staticmethod
+    def price_calc(
+        inventory_items_datagrid="data/import/inventory_items.xlsx",
+        price_rules_datagrid="data/import/price_rules.xlsx",
+        pricelist_datagrid="data/import/pricelist.xlsx",
+        contract_items_datagrid="data/import/contract_items.xlsx",
+        price_changes_report="data/export/price_changes_report.xlsx",
+        pricelist="data/export/pricelist.csv",
+        product_price_task="data/export/product_price_task.txt",
+        contract_item_task="data/export/contract_item_task.txt",
+        tickets_list="data/export/tickets_list.txt"
+    ):
+        session = db_session()
+        import_inventory_items(inventory_items_datagrid, session)
+        import_warehouse_stock_items(inventory_items_datagrid, session)
+        import_price_rules(price_rules_datagrid, session)
+        import_price_region_items(pricelist_datagrid, session)
+        import_contract_items(contract_items_datagrid, session)
+
+        price_region_items = session.query(PriceRegionItem).filter(
+            PriceRegionItem.price_rule_id.isnot(None)
+        ).all()
+        
+        price_changes = recalculate_sell_prices(price_region_items, session)
+        updated_price_region_items = [
+            price_change.price_region_item for price_change in price_changes
+        ]
+        updated_contract_items = recalculate_contract_prices(
+            price_changes, session)
+
+        def updated_default_price_regions():
+            for price_change in price_changes:
+                price_region_item = price_change.price_region_item
+                if price_region_item.code == "":
+                    yield price_region_item
+
+        def warehouse_stock_items_needing_tickets():
+            for price_region_item in updated_default_price_regions():
+                inventory_item = price_region_item.inventory_item
+                warehouse_stock_items = inventory_item.warehouse_stock_items
+                for warehouse_stock_item in warehouse_stock_items:
+                    if warehouse_stock_item.on_hand:
+                        yield warehouse_stock_item
+                    elif warehouse_stock_item.minimum:
+                        yield warehouse_stock_item
+                    elif warehouse_stock_item.bin_location:
+                        yield warehouse_stock_item
+
+        export_price_changes_report(price_changes_report, price_changes)
+        export_pricelist(pricelist, updated_price_region_items)
+        export_product_price_task(product_price_task, updated_price_region_items)
+        export_contract_item_task(contract_item_task, updated_contract_items)
+        export_tickets_list(tickets_list, warehouse_stock_items_needing_tickets())
---
+++ tests/operations.py
@@ -0,0 +1,24 @@
+import pathlib
+
+from pxi.operations import operations
+from tests import DatabaseTestCase
+
+class OperationTests(DatabaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        pathlib.Path("tmp").mkdir(parents=True, exist_ok=True)
+
+    def test_price_calc(self):
+        operations.price_calc(
+            inventory_items_datagrid="tests/fixtures/inventory_items.xlsx",
+            price_rules_datagrid="tests/fixtures/price_rules.xlsx",
+            pricelist_datagrid="tests/fixtures/pricelist.xlsx",
+            contract_items_datagrid="tests/fixtures/contract_items.xlsx",
+            price_changes_report="tmp/price_changes_report.xlsx",
+            pricelist="tmp/pricelist.csv",
+            product_price_task="tmp/product_price_task.txt",
+            contract_item_task="tmp/contract_item_task.txt",
+            tickets_list="tmp/tickets_list.txt"
+        )
+        # raise Exception("Unfinished test.")
