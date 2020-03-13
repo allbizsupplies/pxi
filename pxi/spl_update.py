5773a681941f8f020bd62ce08f2e40364f86e7e0
Brad Treloar <bradley.treloar@gmail.com>
2020-03-13 12:43:02 +1030

    add supplier price updater

---
+++ pxi/spl_update.py
@@ -0,0 +1,84 @@
+import csv
+from decimal import Decimal
+
+from pxi.models import SupplierItem
+
+
+SPL_FIELDNAMES = [
+    "supplier_code",
+    "catalogue_part_no",
+    "supp_item_code",
+    "desc_line_1",
+    "desc_line_2",
+    "supp_uom",
+    "supp_sell_uom",
+    "supp_eoq",
+    "supp_conv_factor",
+    "supp_price_1",
+    "supp_price_2",
+    "supp_price_3",
+    "supp_price_4",
+    "gst",
+    "barcode",
+    "carton_size",
+    "flc_page_no",
+    "rrp",
+    "major_category",
+    "minor_category",
+    "was_manufacturer_code",
+    "item_code",
+    "office_choice_code",
+    "quantity_1_pronto_0",
+    "quantity_2_pronto_1",
+    "quantity_3_pronto_2",
+    "quantity_4_pronto_3",
+    "price_1_pronto_0",
+    "price_2_pronto_1",
+    "price_3_pronto_2",
+    "price_4_pronto_3",
+    "supp_priority",
+    "supp_inner_uom",
+    "supp_inner_barcode",
+    "supp_inner_conversion_factor",
+    "supp_outer_uom",
+    "supp_outer_barcode",
+    "supp_outer_conversion_factor",
+    "unit_measurements",
+    "unit_weight",
+    "cartons_per_pallet",
+    "eoq",
+    "sell_uom",
+    "is_consumable",
+    "is_branded",
+    "is_green",
+    "created_on",
+    "status",
+    "product_class",
+    "product_group",
+]
+
+
+def update_supplier_items(supplier_pricelist_items, session):
+    price_changes = []
+    for item in supplier_pricelist_items:
+        supplier_code = item["supplier_code"]
+        item_code = item["supp_item_code"]
+        buy_price = Decimal(item["supp_price_1"]).quantize(Decimal("0.01"))
+        supplier_items = session.query(SupplierItem).join(
+            SupplierItem.inventory_item
+        ).filter(
+            SupplierItem.code == supplier_code,
+            SupplierItem.item_code == item_code,
+        ).all()
+        if len(supplier_items) == 0:
+            continue
+        for supplier_item in supplier_items:
+            price_diff = supplier_item.buy_price - buy_price
+            if abs(price_diff) > 0:
+                supplier_item.buy_price = buy_price
+                session.commit()
+                price_changes.append({
+                    "supplier_item": supplier_item,
+                    "price_diff": price_diff,
+                })
+    return price_changes
---
+++ tests/spl_update.py
@@ -0,0 +1,39 @@
+from decimal import Decimal
+import random
+
+from pxi.models import SupplierItem
+from pxi.spl_update import update_supplier_items
+from tests import DatabaseTestCase
+from tests.fixtures.models import (
+    random_supplier_item,
+    random_inventory_item)
+
+
+class SPLUpdateTests(DatabaseTestCase):
+
+    def test_update_supplier_items(self):
+        """Apply supplier pricelist prices to supplier items."""
+        item_count = 10
+        supplier_items = []
+        for i in range(item_count):
+            inventory_item = random_inventory_item()
+            self.session.add(inventory_item)
+            supplier_item = random_supplier_item(inventory_item)
+            self.session.add(supplier_item)
+            supplier_items.append(supplier_item)
+
+        def random_supplier_pricelist_item(supplier_item):
+            supplier_code = supplier_item.code
+            item_code = supplier_item.item_code
+            # Mock a price increase by multiplying the existing buy price.
+            buy_price = supplier_item.buy_price * Decimal(random.randint(200, 400)) / 100
+            return {
+                "supplier_code": supplier_code,
+                "supp_item_code": item_code,
+                "supp_price_1": buy_price,
+            }
+        supplier_pricelist_items = [random_supplier_pricelist_item(item)
+            for item in supplier_items]
+
+        price_changes = update_supplier_items(supplier_pricelist_items, self.session)
+        self.assertEqual(10, len(price_changes))
