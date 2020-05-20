7b254857c35d6ca923b9b17b35b4995df5dea2ae
Brad Treloar <brad@treloardigital.com.au>
2020-05-20 13:30:33 +0930

    Updated SPL filename

--- download_spl.sh
+++ download_spl.sh
@@ -3,4 +3,4 @@
 # Download SPL file from Pronto server.
 # Requires that Pronto server be added to SSH config as "pronto"
 supplier_code=$1
-scp "pronto:/oclimport/supplierpricelist/SPL2019${supplier_code}.PRN" "data/import/supplier_pricelist.csv"
+scp "pronto:/oclimport/supplierpricelist/SPL${supplier_code}.PRN" "data/import/supplier_pricelist.csv"
