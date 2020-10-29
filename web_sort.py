b18bb116aaa00f74e31f24e0129a4c21a1bd25ab
Brad Treloar <brad@allbizsupplies.biz>
2020-10-29 15:00:48 +1030

    add all kwargs

--- web_sort.py
+++ web_sort.py
@@ -10,6 +10,9 @@ if not sys.warnoptions:
 
 operations.web_sort(
     inventory_items_datagrid="data/import/inventory_items.xlsx",
+    price_rules_datagrid="data/import/price_rules.xlsx",
+    pricelist_datagrid="data/import/pricelist.xlsx",
     inventory_metadata="data/import/inventory_metadata.xlsx",
     product_web_sortcode_task="data/export/product_web_sortcode_task.txt",
+    product_web_sortcode_report="data/export/product_web_sortcode_report.xlsx"
 )
