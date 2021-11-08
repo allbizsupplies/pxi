ac65bacb698488014fb47f130b82872d4ad612ca
Brad Treloar <brad@treloardigital.com.au>
2021-11-08 11:33:44 +1030

    remove bash scripts

--- download_spl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Download SPL file from Pronto server.
-# Requires that Pronto server be added to SSH config as "pronto"
-supplier_code=$1
-scp "pronto:/oclimport/supplierpricelist/SPL${supplier_code}.PRN" "data/import/supplier_pricelist.csv"
--- upload_pricelist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp data/export/pricelist.csv pronto:/home/albbrad/import/pricelist.csv
-
