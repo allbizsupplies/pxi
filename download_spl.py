ef53868ab7bfe6e4455fcfa5dbfa2b20ac70817f
Brad Treloar <brad@treloardigital.com.au>
2021-11-08 11:33:28 +1030

    remove obsolete code replacement

--- download_spl.py
+++ download_spl.py
@@ -17,11 +17,3 @@ password = os.getenv("PRONTO_PASSWORD")
 supplier_code = sys.argv[1] if len(sys.argv) >= 2 else ""
 scp_client = get_scp_client(hostname, username, password)
 scp_client.get(REMOTE_FILE.format(supplier_code), LOCAL_FILE)
-
-# Convert GNS SPL to SAT SPL
-if supplier_code == "SAT":
-    with open(LOCAL_FILE, "r") as file:
-        lines = file.readlines()
-    new_lines = [line.replace("GNS", "SAT") for line in lines]
-    with open(LOCAL_FILE, "w") as file:
-        file.writelines(new_lines)
