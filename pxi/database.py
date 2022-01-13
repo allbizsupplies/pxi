7a892747e4bea11ef71d2c944159589f91a6d450
Brad Treloar <brad@treloardigital.com.au>
2022-01-13 11:46:37 +1030

    create database module

---
+++ pxi/database.py
@@ -0,0 +1,12 @@
+from os import PathLike
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from pxi.models import Base
+
+
+def get_session(sqlite_filepath: PathLike):
+    db = create_engine(f"sqlite:///{sqlite_filepath}")
+    Base.metadata.create_all(db)
+    session = sessionmaker(bind=db)()
+    return session
