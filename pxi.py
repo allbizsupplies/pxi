892fc87424afefabd56b673cb53168889545a9eb
Brad Treloar <brad@treloardigital.com.au>
2022-01-13 12:18:48 +1030

    add pxi.py script

---
+++ pxi.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+from argparse import ArgumentParser
+import sys
+import logging
+from time import perf_counter
+import yaml
+
+from pxi.commands import Commands
+
+# Suppress warnings.
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+
+def main():
+    start_at = perf_counter()
+    args = get_args()
+    with open(args.config) as file:
+        config = yaml.safe_load(file)
+    # Configure logging.
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    if args.verbose:
+        logging.basicConfig(
+            format="%(levelname)s: %(message)s",
+            level=logging_level)
+    else:
+        logging.basicConfig(
+            filename=config["paths"]["logging"],
+            format="%(asctime)s %(levelname)s: %(message)s",
+            level=logging_level)
+    try:
+        command = getattr(Commands, args.command)
+        logging.info(f"Command: {args.command}")
+        command(config)(
+            force_imports=args.force_imports)
+        duration = (perf_counter() - start_at) * 1000
+        logging.info(f"{args.command} ({int(duration)}ms)")
+    except AttributeError as ex:
+        print("Error: Command does not exist: " + args.command)
+        logging.error("Command does not exist: " + args.command)
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument("command",
+                        help="the command to execute")
+    parser.add_argument("--config",
+                        help="path to config",
+                        default="config.yml")
+    parser.add_argument("--debug",
+                        help="print debugging logs",
+                        dest="debug", action="store_true")
+    parser.add_argument("--verbose",
+                        help="print logs to terminal",
+                        dest="verbose", action="store_true")
+    parser.add_argument("--force-imports",
+                        help="force all data to be imported",
+                        dest="force_imports", action="store_true")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pxi/commands.py
@@ -0,0 +1,157 @@
+import asyncio
+from datetime import datetime
+from decimal import Decimal
+import logging
+from time import perf_counter
+
+from pxi.database import get_session
+from pxi.enum import ItemCondition, ItemType
+from pxi.exporters import (
+    export_contract_item_task,
+    export_price_changes_report,
+    export_pricelist,
+    export_product_price_task,
+    export_tickets_list,
+)
+from pxi.importers import import_data
+from pxi.models import ContractItem, InventoryItem, PriceRegionItem, PriceRule, WarehouseStockItem
+from pxi.price_calc import (
+    recalculate_contract_prices,
+    recalculate_sell_prices
+)
+from pxi.scp import get_scp_client
+
+
+class CommandBase:
+
+    def __init__(self, config):
+        self.config = config
+        self.db_session = get_session(config["paths"]["database"])
+
+    def __call__(self, **options):
+        self.execute(options)
+
+
+class CommandError(BaseException):
+    pass
+
+
+class Commands:
+
+    class help(CommandBase):
+        """Displays help text."""
+
+        def execute(self, options):
+            print("\nAvailable commands:\n")
+            commands = []
+            for attr_name in dir(Commands):
+                attr = getattr(Commands, attr_name)
+                if type(attr).__name__ == "type" and attr.__base__.__name__ == "CommandBase":
+                    commands.append(attr)
+            for command in commands:
+                print(f"{command.__name__:<24}{command.__doc__}")
+            print()
+
+    class price_calc(CommandBase):
+        """Calculate rounded prices for price region items."""
+
+        def execute(self, options):
+            force_imports = False
+            if "force_imports" in options:
+                force_imports = options["force_imports"]
+            import_data(self.db_session, self.config["paths"]["import"], [
+                InventoryItem,
+                WarehouseStockItem,
+                PriceRule,
+                PriceRegionItem,
+                ContractItem,
+            ], force_imports=True)
+            # Select all price region items that have a price rule and belong to
+            # an active inventory item.
+            # pylint:disable=no-member
+            price_region_items = self.db_session.query(PriceRegionItem).join(
+                PriceRegionItem.inventory_item
+            ).join(
+                PriceRegionItem.price_rule
+            ).filter(
+                PriceRegionItem.price_rule_id.isnot(None),
+                ~PriceRule.code.in_(self.config["price_rules"]["ignore"]),
+                InventoryItem.condition != ItemCondition.DISCONTINUED,
+                InventoryItem.condition != ItemCondition.INACTIVE,
+                InventoryItem.item_type != ItemType.CROSS_REFERENCE,
+                InventoryItem.item_type != ItemType.LABOUR,
+                InventoryItem.item_type != ItemType.INDENT_ITEM
+            ).all()
+            price_changes = recalculate_sell_prices(
+                price_region_items, self.db_session)
+            updated_price_region_items = [
+                price_change.price_region_item for price_change in price_changes
+            ]
+            updated_contract_items = recalculate_contract_prices(
+                price_changes, self.db_session)
+
+            def updated_default_price_regions():
+                for price_change in price_changes:
+                    price_region_item = price_change.price_region_item
+                    in_default_price_region = price_region_item.code != ""
+                    price_has_changed = price_change.price_diffs[0] < Decimal(
+                        "0.005")
+                    if in_default_price_region and price_has_changed:
+                        yield price_region_item
+
+            def ticketed_warehouse_stock_items():
+                for price_region_item in updated_default_price_regions():
+                    inventory_item = price_region_item.inventory_item
+                    warehouse_stock_items = inventory_item.warehouse_stock_items
+                    for warehouse_stock_item in warehouse_stock_items:
+                        if warehouse_stock_item.on_hand > 0:
+                            yield warehouse_stock_item
+                        elif warehouse_stock_item.minimum > 0:
+                            yield warehouse_stock_item
+                        elif warehouse_stock_item.bin_location:
+                            ignored_bin_locations = self.config["bin_locations"]["ignore"]
+                            if warehouse_stock_item.bin_location not in ignored_bin_locations:
+                                yield warehouse_stock_item
+
+            export_paths = self.config["paths"]["export"]
+            export_price_changes_report(
+                export_paths["price_changes_report"], price_changes)
+            export_pricelist(
+                export_paths["pricelist"], updated_price_region_items)
+            export_product_price_task(
+                export_paths["product_price_task"], updated_price_region_items)
+            export_contract_item_task(
+                export_paths["contract_item_task"], updated_contract_items)
+            export_tickets_list(
+                export_paths["tickets_list"], ticketed_warehouse_stock_items())
+            logging.info("")
+
+    class download_spl(CommandBase):
+        """Download supplier pricelist from remote server."""
+
+        def execute(self, options):
+            config = self.config["ssh"]
+            scp_client = get_scp_client(
+                config["hostname"], config["username"], config["password"])
+            scp_client.get(self.config["paths"]["remote"]["supplier_pricelist"],
+                           self.config["paths"]["import"]["supplier_pricelist"])
+
+    class upload_spl(CommandBase):
+        """Upload supplier pricelist to remote server."""
+
+        def execute(self, options):
+            config = self.config["ssh"]
+            scp_client = get_scp_client(
+                config["hostname"], config["username"], config["password"])
+            scp_client.put(self.config["paths"]["export"]["supplier_pricelist"],
+                           self.config["paths"]["remote"]["supplier_pricelist_import"])
+
+    class upload_pricelist(CommandBase):
+        """Upload pricelist to remote server."""
+
+        def execute(self, options):
+            config = self.config["ssh"]
+            scp_client = get_scp_client(
+                config["hostname"], config["username"], config["password"])
+            scp_client.put(self.config["paths"]["export"]["pricelist"],
+                           self.config["paths"]["remote"]["pricelist"])
