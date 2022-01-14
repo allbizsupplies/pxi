5ea19d427757fe4ade58ffaf3a0da55aee4dd350
Brad Treloar <brad.treloar@pm.me>
2022-01-14 12:53:21 +1030

    remove test for deleted fetcher

--- tests/fetchers.py
+++ tests/fetchers.py
@@ -51,28 +51,3 @@ class FetcherTests(DatabaseTestCase):
             self.assertTrue(os.path.exists(filepath))
             self.assertEqual(expected_filepath, filepath)
             os.remove(filepath)
-
-    def test_AVD_fetch_url(self):
-        """Fetch image URL from Avery website."""
-        inventory_item = random_inventory_item()
-        supplier_item = random_supplier_item(inventory_item)
-        supplier_item.code = "AVD"
-        supplier_item.item_code = random_string(20)
-        # pylint:disable=no-member
-        self.session.commit()
-        fetcher = get_fetchers(inventory_item)[0]
-        url = fetcher.get_image_url(inventory_item)
-        self.assertIsNone(url)
-
-        inventory_item = random_inventory_item()
-        supplier_item = random_supplier_item(inventory_item)
-        supplier_item.code = "AVD"
-        supplier_item.item_code = "44540"
-        fetchers = get_fetchers(inventory_item)
-        self.assertEqual(1, len(fetchers))
-        self.session.commit()
-        fetcher = get_fetchers(inventory_item)[0]
-        expected_url = "https://www.averyproducts.com.au/sites/avery.au/files/styles/scale_1_1_ratio_style/public/avery_importer/media/44540_pac.jpg?itok=L9_umi6U"
-        fetcher = get_fetchers(inventory_item)[0]
-        url = fetcher.get_image_url(inventory_item)
-        self.assertEqual(url, expected_url)
