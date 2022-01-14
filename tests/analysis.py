409e882dc0fd41f5411b01f079138cfdb745db3a
Brad Treloar <brad.treloar@pm.me>
2022-01-14 13:59:52 +1030

    refactor tests

--- tests/analysis.py
+++ tests/analysis.py
@@ -15,43 +15,28 @@ from tests.fixtures.models import random_inventory_item
 class AnalysisTests(DatabaseTestCase):
 
     def test_extract_terms(self):
-        descriptions = [
-            "Item 1 of 5",
-            "Item  2",
-            "Item 3 50 x 60 cm",
-            "Item 4 50x60cm",
-            "Item 5 and 6",
+        fixtures = [
+            ("Item 1 of 5", ["item", "1", "5"]),
+            ("Item  2", ["item", "2"]),
+            ("Item 3 50 x 60 cm", ["item", "3", "50", "60", "cm"]),
+            ("Item 4 50x60cm", ["item", "4", "50x60cm"]),
+            ("Item 5 and 6", ["item", "5", "6"]),
         ]
 
-        expected_results = [
-            ["item", "1", "5"],
-            ["item", "2"],
-            ["item", "3", "50", "60", "cm"],
-            ["item", "4", "50x60cm"],
-            ["item", "5", "6"],
-        ]
-
-        for i in range(5):
-            result = extract_terms(descriptions[i])
-            self.assertEqual(expected_results[i], result)
+        for string, expected_result in fixtures:
+            self.assertEqual(expected_result, extract_terms(string))
 
     def test_compare_terms(self):
         subject = ["A", "B", "C"]
-        comparisons = [
-            ["A", "B"],
-            ["A", "C", "B"],
-            ["A", "B", "C", "D"],
+
+        fixtures = [
+            (["A", "B"], {"hits": 2, "direct_hits": 2, "misses": 1}),
+            (["A", "C", "B"], {"hits": 3, "direct_hits": 1, "misses": 0}),
+            (["A", "B", "C", "D"], {"hits": 3, "direct_hits": 3, "misses": 1}),
         ]
 
-        expected_results = [
-            {"hits": 2, "direct_hits": 2, "misses": 1},
-            {"hits": 3, "direct_hits": 1, "misses": 0},
-            {"hits": 3, "direct_hits": 3, "misses": 1},
-        ]
-
-        for i in range(3):
-            result = compare_terms(subject, comparisons[i])
-            self.assertEqual(expected_results[i], result)
+        for inputs, expected_result in fixtures:
+            self.assertEqual(expected_result, compare_terms(subject, inputs))
 
     def test_calculate_similarity(self):
         inputs = [
@@ -61,8 +46,9 @@ class AnalysisTests(DatabaseTestCase):
         ]
 
         for input in inputs:
-            expected_result = input["hits"] * HIT_MULTIPLIER + input["direct_hits"] * \
-                DIRECT_HIT_MULTIPLIER - input["misses"] * MISS_MULTIPLIER
+            expected_result = input["hits"] * HIT_MULTIPLIER \
+                + input["direct_hits"] * DIRECT_HIT_MULTIPLIER \
+                - input["misses"] * MISS_MULTIPLIER
             result = calculate_similarity(
                 input["hits"], input["direct_hits"], input["misses"])
             self.assertEqual(expected_result, result)
