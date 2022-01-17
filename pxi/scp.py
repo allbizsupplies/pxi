c17563eb932e9c47f6340fadbacb5d90e65bd334
Brad Treloar <brad.treloar@pm.me>
2022-01-17 15:59:56 +1030

    add tests

--- pxi/scp.py
+++ pxi/scp.py
@@ -2,6 +2,7 @@ from paramiko import AutoAddPolicy, SSHClient
 from scp import SCPClient
 import sys
 
+
 def get_scp_client(hostname, username, password):
     ssh_client = SSHClient()
     ssh_client.set_missing_host_key_policy(AutoAddPolicy())
--- tests/commands.py
+++ tests/commands.py
@@ -1,7 +1,34 @@
 
+from unittest.mock import MagicMock, patch
 
 from tests import DatabaseTestCase
 from pxi.commands import Commands, get_command
+
+
+def get_mock_config():
+    return {
+        "paths": {
+            "database": ":memory:",
+            "import": {
+                "supplier_pricelist": "path/import/supplier_pricelist",
+                "pricelist": "path/import/pricelist",
+            },
+            "export": {
+                "supplier_pricelist": "path/export/supplier_pricelist",
+                "pricelist": "path/export/pricelist",
+            },
+            "remote": {
+                "pricelist": "path/remote/pricelist",
+                "supplier_pricelist": "path/remote/supplier_pricelist",
+                "supplier_pricelist_import": "path/remote/supplier_pricelist_import",
+            }
+        },
+        "ssh": {
+            "hostname": "pronto.example.com",
+            "username": "prontousername",
+            "password": "prontopassword",
+        }
+    }
 
 
 class CommandTests(DatabaseTestCase):
@@ -26,3 +53,41 @@ class CommandTests(DatabaseTestCase):
 
         for command_name, expected_command in fixtures:
             self.assertEqual(expected_command, get_command(command_name))
+
+    def test_command_help(self):
+        mock_config = get_mock_config()
+        command = Commands.help(mock_config)
+        command()
+
+    @patch("pxi.commands.get_scp_client")
+    def test_command_download_spl(self, mock_get_scp_client):
+        mock_config = get_mock_config()
+        mock_scp_client = MagicMock()
+        mock_get_scp_client.return_value = mock_scp_client
+        command = Commands.download_spl(mock_config)()
+        mock_get_scp_client.assert_called_with(*mock_config["ssh"].values())
+        mock_scp_client.get.assert_called_with(
+            mock_config["paths"]["remote"]["supplier_pricelist"],
+            mock_config["paths"]["import"]["supplier_pricelist"])
+
+    @patch("pxi.commands.get_scp_client")
+    def test_command_upload_spl(self, mock_get_scp_client):
+        mock_config = get_mock_config()
+        mock_scp_client = MagicMock()
+        mock_get_scp_client.return_value = mock_scp_client
+        command = Commands.upload_spl(mock_config)()
+        mock_get_scp_client.assert_called_with(*mock_config["ssh"].values())
+        mock_scp_client.put.assert_called_with(
+            mock_config["paths"]["export"]["supplier_pricelist"],
+            mock_config["paths"]["remote"]["supplier_pricelist_import"])
+
+    @patch("pxi.commands.get_scp_client")
+    def test_command_upload_pricelist(self, mock_get_scp_client):
+        mock_config = get_mock_config()
+        mock_scp_client = MagicMock()
+        mock_get_scp_client.return_value = mock_scp_client
+        command = Commands.upload_pricelist(mock_config)()
+        mock_get_scp_client.assert_called_with(*mock_config["ssh"].values())
+        mock_scp_client.put.assert_called_with(
+            mock_config["paths"]["export"]["pricelist"],
+            mock_config["paths"]["remote"]["pricelist"])
